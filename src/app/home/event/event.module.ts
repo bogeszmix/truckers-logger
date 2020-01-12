1b765646521d0eb6d0d0a3e875e363aa46ae7928
Bognár Olivér <bognar.oliver.attila@gmail.com>
2020-01-12 21:59:06 +0100

    #5: Responsive table and some fix.
    Responsive table and some fix and deletions.

--- src/app/home/event/event.module.ts
+++ src/app/home/event/event.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms' ;
 import { NgbDatepickerModule  } from '@ng-bootstrap/ng-bootstrap';
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
-import { from } from 'rxjs';
 import { CreateEventComponent } from './components/create-event/create-event.component';
 import { QuickFilteredStatisticsComponent } from './components/quick-filtered-statistics/quick-filtered-statistics.component';
 import { EventListComponent } from './components/event-list/event-list.component';
